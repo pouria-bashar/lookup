8f71870ebab37387f3b2ebaaeb894fc767bdd735
Pouria <pouria.bashardoust@gmail.com>
2016-03-27 09:44:57 +1100

    Added query param

--- src/com/pba/lookup/web/asic/CompanySearch.java
+++ src/com/pba/lookup/web/asic/CompanySearch.java
@@ -1,8 +1,10 @@
 package com.pba.lookup.web.asic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pba.lookup.service.LookupService;
@@ -12,9 +14,10 @@ public class CompanySearch {
 
 	@Autowired
 	private LookupService lookupService;
-	@RequestMapping(value = "abc" , method = RequestMethod.GET)
-	public String getResponse(){
+	
+	@RequestMapping(value = "ASIC/search" , method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getResponse(@RequestParam("name") String name){
 		
-		return lookupService.getCompanyByName("PBA").toString();
+		return lookupService.getCompanyByName(name).toString();
 	}
 }
